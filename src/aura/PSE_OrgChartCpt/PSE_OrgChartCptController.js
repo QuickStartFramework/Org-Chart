65bf8c8dad12d10454232f2813f6d33e88896d51
Maxime Jacquet <mjacquet@salesforce.com>
2017-01-02 14:18:56 +0100

    Cleaned all warnings - LockerService linting.

--- src/aura/PSE_OrgChartCpt/PSE_OrgChartCptController.js
+++ src/aura/PSE_OrgChartCpt/PSE_OrgChartCptController.js
@@ -1,42 +1,41 @@
 ({
     doInit : function(component, event, helper) {
-        helper.loadTree(component,null);
+        helper.loadTree(component, null);
     },
-    toggleFlatList : function(component, event, helper) {
+    toggleFlatList : function(component) {
        // console.log('Toggle Flat List');
-        component.set("v.flat",!component.get("v.flat"));
-        component.set("v.search",false); // Hide Search area
+        component.set("v.flat", !component.get("v.flat"));
+        component.set("v.search", false); // Hide Search area
     },
-    toggleSearch : function(component, event, helper) {
+    toggleSearch : function(component) {
        // console.log('Toggle Search');
-        component.set("v.search",!component.get("v.search"));
+        component.set("v.search", !component.get("v.search"));
     },
     selectNode : function(component, event, helper) {
         var nodeId=event.target.getAttribute("data-id");
        // console.log('select Node '+nodeId);
-        helper.loadTree(component,nodeId);
+        helper.loadTree(component, nodeId);
     },
     prevSibling : function(component, event, helper) {
         var nodeId=component.get("v.prevSibling").Id;
-        helper.loadTree(component,nodeId);
-    }, 
+        helper.loadTree(component, nodeId);
+    },
     nextSibling : function(component, event, helper) {
         var nodeId=component.get("v.nextSibling").Id;
-        helper.loadTree(component,nodeId);
-    }, 
+        helper.loadTree(component, nodeId);
+    },
     doSearch : function(component, event, helper) {
-        var SearchInput = component.find("txtSearch"); 
+        var SearchInput = component.find("txtSearch");
         var strSearchText = SearchInput.get("v.value");
         helper.userSearch(component, strSearchText + '*');
     },
-	navigateToSelectedUser : function(component, event, helper) {
-        var sObjId = component.get("v.currentUser.Id");          
+	navigateToSelectedUser : function(component) {
+        var sObjId = component.get("v.currentUser.Id");
         var navToSObjEvt = $A.get("e.force:navigateToSObject");
         navToSObjEvt.setParams({
             recordId: sObjId,
             slideDevName: "detail"
-        }); 
-        navToSObjEvt.fire(); 
+        });
+        navToSObjEvt.fire();
     }
-
 })
--- src/aura/PSE_OrgChartCpt/PSE_OrgChartCptHelper.js
+++ src/aura/PSE_OrgChartCpt/PSE_OrgChartCptHelper.js
@@ -2,7 +2,7 @@
     // =========================================
     // LOAD TREE
     // Entry : Id userId
-    // Set : 
+    // Set :
     // - USER currentUser 	: USER record of id :userId
     // - USER[] listManagers 	: Management chain above :userId
     // - USER[] listReports 	: All reports of :userId
@@ -12,40 +12,44 @@
     // ==================================================
     loadTree : function(component, userId) {
         var loadHierarchy = component.get("c.getHierarchy");
-        if (userId) loadHierarchy.setParams({ userid : userId });
-        component.set("v.displayLoader",true); 
+        if (userId){
+            loadHierarchy.setParams({ userid : userId });
+        }
+        component.set("v.displayLoader", true);
         loadHierarchy.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
                // console.log(response.getReturnValue());
                 component.set("v.currentUser", response.getReturnValue().selectedUser);
                 component.set("v.listManagers", response.getReturnValue().listManagers);
-                component.set("v.listReports", response.getReturnValue().listReports); 
-                component.set("v.nextSibling", response.getReturnValue().nextSibling); 
-                component.set("v.prevSibling", response.getReturnValue().prevSibling); 
+                component.set("v.listReports", response.getReturnValue().listReports);
+                component.set("v.nextSibling", response.getReturnValue().nextSibling);
+                component.set("v.prevSibling", response.getReturnValue().prevSibling);
                 // Circle Reports
                 var reports=response.getReturnValue().listReports;
                 var listReports=[];
                 var circleReports=[];
-                if (reports.length>0)
-                for (var i=0;i<10&i<reports.length;i++) { // Ellipse 150w, 120h
-                    listReports.push({});
-                    listReports[i].user=reports[i];
-                   // listReports[i].left=-160*Math.cos(i*(Math.PI)/(10-1)); 
-                  //  listReports[i].top=120*Math.sin(i*(Math.PI) /(10-1));
-                   // listReports[i].left=1-(); //120-120*Math.cos(i*(Math.PI)/(reports.length-1)); 
-                    listReports[i].top=10*reports.length*Math.sin(((i/(reports.length-1))*(Math.PI)));
-                    circleReports.push(listReports[i]);
+                if (reports.length>0){
+                    for (var i=0;i<10;i+=1) { // Ellipse 150w, 120h
+                        if(i >= reports.length){
+                            break;
+                        }
+                        listReports.push({});
+                        listReports[i].user=reports[i];
+                       // listReports[i].left=-160*Math.cos(i*(Math.PI)/(10-1));
+                      //  listReports[i].top=120*Math.sin(i*(Math.PI) /(10-1));
+                       // listReports[i].left=1-(); //120-120*Math.cos(i*(Math.PI)/(reports.length-1));
+                        listReports[i].top=10*reports.length*Math.sin(((i/(reports.length-1))*(Math.PI)));
+                        circleReports.push(listReports[i]);
+                    }
                 }
-                
-                component.set("v.circleReports", circleReports); 
+                component.set("v.circleReports", circleReports);
                // console.log(circleReports);
                 // Collapse flat list, hide loader and search areas
-                component.set("v.flat",false);
-                component.set("v.displayLoader",false);
-                component.set("v.search",false);
-            }
-            else if (state === "ERROR") {
+                component.set("v.flat", false);
+                component.set("v.displayLoader", false);
+                component.set("v.search", false);
+            }else if (state === "ERROR") {
                 var errors = response.getError();
                 if (errors) {
                     if (errors[0] && errors[0].message) {
@@ -60,18 +64,18 @@
     },
     // ========================================================================
     // USER SEARCH
-    // Entry : String inSearchStr 
-    // Set : 
+    // Entry : String inSearchStr
+    // Set :
     // - USER[] searchResults	: List of matching records (SOSL Query)
     // ========================================================================
     userSearch : function(component, inSearchStr) {
         var usrSearch = component.get("c.getUserSearch");
       //  console.log('User Search Search String: ' + inSearchStr);
-        component.set("v.displayLoader",true);
-        if (inSearchStr !== '') {          
+        component.set("v.displayLoader", true);
+        if (inSearchStr !== ''){
             usrSearch.setParams({inSearchStr: inSearchStr});
             usrSearch.setCallback(this, function(response){
-                component.set("v.displayLoader",false);
+                component.set("v.displayLoader", false);
                 var state = response.getState();
                 if (state === "SUCCESS") {
                     component.set("v.searchResults", response.getReturnValue());
@@ -87,6 +91,6 @@
             }
             });
             $A.enqueueAction(usrSearch);
-        } 
+        }
     }
 })
